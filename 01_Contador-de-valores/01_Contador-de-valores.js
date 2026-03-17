c8ada51d00c8c536010b628d3efd4d34d21571b8
Samuel Cunda <cundasamuel@gmail.com>
2026-03-17 08:46:29 -0300

    finish

--- 01_Contador-de-valores/01_Contador-de-valores.js
+++ 01_Contador-de-valores/01_Contador-de-valores.js
@@ -1,4 +1,3 @@
-/*Declarei as variaveis */
 const valor = document.getElementById('valor') ;
 const mais = document.getElementById('mais');
 const menos = document.getElementById('menos');
@@ -6,19 +5,16 @@ const resetar = document.getElementById('reset');
 
 let contador = 0;
 
-/*Adiciona um numero ao valor*/
 mais.addEventListener('click', () => {
     contador = contador +1;
     valor.innerHTML = contador;
 });
 
-/*Subtrai um numero ao valor*/
 menos.addEventListener('click', () => {
     contador = contador -1;
     valor.innerHTML = contador;
 });
 
-/*Reseta o valor do contador*/
 resetar.addEventListener('click', () => {
     contador= 0
     valor.innerHTML = contador
--- 02_Lampada/02_Lampada.js
+++ 02_Lampada/02_Lampada.js
@@ -1,15 +1,12 @@
-/*Declarei as Variaveis*/
 const botaoLigado = document.getElementById('botaoLigado');
 const botaoDesligado = document.getElementById('botaoDesligado');
 let Imagem = document.getElementById('imagem');
 
-/*Remove a ligada e adiciona a desligada*/
 botaoDesligado.addEventListener('click', () => {
     Imagem.classList.remove('ligada');
     Imagem.classList.add('desligada');
 });
 
-/*Remove a desligada e adiciona a ligada*/
 botaoLigado.addEventListener('click', () => {
     Imagem.classList.add('ligada');
     Imagem.classList.remove('desligada');
